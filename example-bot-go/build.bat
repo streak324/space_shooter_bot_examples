2631efda79526314c3c0353e636d444439cdcc99
Andrew Sepulveda <streak324@gmail.com>
2024-05-26 09:44:53 -0700

    example printing

--- example-bot-go/build.bat
+++ example-bot-go/build.bat
@@ -12,5 +12,6 @@ set "OUTPUT_NAME=bot"
 echo Building for %GOOS%/%GOARCH%
 set GOOS=%GOOS%
 set GOARCH=%GOARCH%
-tinygo build -o %OUTPUT_NAME%.wasm -target wasm-unknown .
+:: do not use opt=z which is the default. may cause some unused wasm imports to be removed, which the wasm host would not like
+tinygo build -o %OUTPUT_NAME%.wasm --opt 1 -target wasm-unknown .
 wasm2wat %OUTPUT_NAME%.wasm -o %OUTPUT_NAME%.wat
--- example-bot-go/main.go
+++ example-bot-go/main.go
@@ -1,11 +1,31 @@
 package main
 
-//export run
-func run(a, b int) int {
-	for b != 0 {
-		a, b = b, a%b
-	}
-	return a
+import "unsafe"
+
+type Entity struct {
+	My    bool   `json:"my"`
+	Id    uint64 `json:"id"`
+	Owner int32  `json:"owner"`
+}
+
+type GameState struct {
+	Entities []Entity `json:"entities"`
+}
+
+var printBuffer [1024]byte
+
+//go:wasmimport host printInt
+func printInt(integer int32)
+
+//go:wasmimport host print
+func print(ptr uint32, len uint32)
+
+//go:export step
+func step() {
+	printInt(1235)
+	copyLength := copy(printBuffer[:], []byte("Hello WASM Host!!!"))
+	intptr := uint32(uintptr(unsafe.Pointer(&printBuffer[0])))
+	print(intptr, uint32(copyLength))
 }
 
 // main is required for the `wasi` target, even if it isn't used.
