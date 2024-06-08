9fb8fcc344764aaec1aa21c001e8d2a5fb0c1e11
Andrew Sepulveda <streak324@gmail.com>
2024-06-08 14:46:53 -0700

    update imports

--- example-bot-go/wasm_imports.go
+++ example-bot-go/wasm_imports.go
@@ -5,7 +5,7 @@ package main
 //
 // passed in parameters is the buffer containing the string, and the byte size
 //
-//go:wasmimport env log
+//go:wasmimport env botsLog
 func _log(ptr, size uint32)
 
 // _getGameState retrieves the current game state and writes it to the provided buffer.
@@ -17,12 +17,12 @@ func _log(ptr, size uint32)
 // if returned byte size is larger than the buffer capacity, then consider the game state has failed to be retrieved.
 // reallocate buffer size to at least as large as the returned byte size.
 //
-//go:wasmimport env getGameState
+//go:wasmimport env botsGetGameState
 func _getGameState(ptr uint32, capacity uint32) int32
 
 // command entity with id `entityId` to move to position `x`, `y`
 //
-//go:wasmimport env moveEntityToTarget
+//go:wasmimport env botsMoveEntityToTarget
 func moveEntityToTarget(entityId uint64, x float32, y float32) int32
 
 // command entity with id `entityId` to rotate its block at index `blockIndex` at `rotation` radians.
@@ -31,19 +31,19 @@ func moveEntityToTarget(entityId uint64, x float32, y float32) int32
 //
 // returns non-zero on parameter validation error
 //
-//go:wasmimport env orientTurret
+//go:wasmimport env botsOrientTurret
 func orientTurret(entityId uint64, blockIndex uint32, rotation float32) int32
 
 // command entity with id `entityId` to fire cannon on block index `blockIndex`
 //
 // returns non-zero on parameter validation error
 //
-//go:wasmimport env fireCannon
+//go:wasmimport env botsFireCannon
 func fireCannon(entityId uint64, blockIndex uint32) int32
 
 // command entity with id `entityId` to launch missiles on block index `blockIndex`
 //
 // returns non-zero on parameter validation error
 //
-//go:wasmimport env launchMissiles
+//go:wasmimport env botsLaunchMissiles
 func launchMissiles(entityId uint64, blockIndex uint32) int32
