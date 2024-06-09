8a5c3584f99a2aaeb3ce65bc272703c7a10b9a44
streak324 <streak324@gmail.com>
2024-06-09 16:01:00 -0700

    get c bot moving around

--- c-bot/build.sh
+++ c-bot/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
-#idk how to get clang to compile with wasm
-#clang --target=wasm32-unknown-unknown --no-standard-libraries -Wl,--export-all -Wl,--no-entry -o bot.wasm main.c
-
-emcc main.c -I./ -o bot.wasm -s WASM=1 -s INITIAL_HEAP=256kb -s STACK_SIZE=64kb -s ALLOW_MEMORY_GROWTH=0 \
+#FLATCC_NO_ASSERT=1 or FLATCC_ASSERT=my_assert is required to avoid WASI imports
+emcc main.c -DFLATCC_ASSERT=my_assert -I./ -o bot.wasm \
+	-s WASM=1 -s INITIAL_HEAP=256kb -s STACK_SIZE=64kb -s ALLOW_MEMORY_GROWTH=0 \
 	-Wl,--stack-first -Wl,--no-demangle -Wl,--no-entry -Wl,--import-memory
 
 wasm2wat bot.wasm > bot.wat
--- c-bot/main.c
+++ c-bot/main.c
@@ -1,6 +1,10 @@
+#define my_assert(expr) if (!(expr)) { *(volatile int*)0 = 0; }
+
 #include <stdint.h>
 #include "game_state_reader.h"
 #include <emscripten.h>
+#include <stdio.h>
+#include <math.h>
 
 typedef int8_t i8;
 typedef int32_t i32;
@@ -10,9 +14,6 @@ typedef uint64_t u64;
 
 typedef float f32;
 typedef double f64;
-
-#undef ns
-#define ns(x) FLATBUFFERS_WRAP_NAMESPACE(MyGameState, x) // Specified in the schema.
 
 //any imported wasm host functions need the function declared with EM_IMPORT(func_name) appended to it. no need to define function since it is provided by the wasm game host
 void botsLog(void* buffer_ptr, i32 length) EM_IMPORT(botsLog);
@@ -66,6 +67,16 @@ typedef struct {
 
 Vec start_position = {};
 
+#define NUM_GOTO_POINTS 4
+Vec goto_points[NUM_GOTO_POINTS] = {
+	{ 100.0f, 0.0f },	
+	{ 0.0f, -100.0f },
+	{ -100.0f, 0.0f },
+	{ 0.0f, -100.0f },
+};
+
+i32 goto_index = 0;
+
 EMSCRIPTEN_KEEPALIVE void step()
 {
 	if (step_count == 0) {
@@ -74,27 +85,44 @@ EMSCRIPTEN_KEEPALIVE void step()
 		game_state_buffer.offset = 0;
 		game_state_buffer.capacity = game_state_buffer_capacity;
 	}
+
+	i32 byte_size = botsGetGameState(game_state_buffer.ptr, game_state_buffer.capacity);
+	my_assert(byte_size > game_state_buffer.capacity);
+
 	GameState_table_t game_state = GameState_as_root((void*) game_state_buffer.ptr);
 	Entity_vec_t entities = GameState_entities_get(game_state);
-	//size_t num_entities = Entity_vec_len(entities);
+	size_t num_entities = Entity_vec_len(entities);
 
-	//i32 i = 0;
-	//for (i = 0; i < num_entities; i++) {
-	//	Entity_table_t entity = Entity_vec_at(entities, i);
-	//	flatbuffers_bool_t is_mine = Entity_my_get(entity);
-	//	if (is_mine) {
-	//		Vec2_table_t position = Entity_position_get(entity);
-	//		if (step_count == 0) {
-	//			start_position.x = Vec2_x_get(position);
-	//			start_position.y = Vec2_y_get(position);
-	//		}
-	//		botsMoveEntityToTarget(Entity_id_get(entity), 0.0f, 0.0f);
-	//	}
-	//}
+	i32 i = 0;
+	for (i = 0; i < num_entities; i++) {
+		Entity_table_t entity = Entity_vec_at(entities, i);
+		flatbuffers_bool_t is_mine = Entity_my_get(entity);
+		if (is_mine) {
+			Vec2_table_t position = Entity_position_get(entity);
+			if (step_count == 0) {
+				start_position.x = Vec2_x_get(position);
+				start_position.y = Vec2_y_get(position);
+			}
+
+			Vec goto_point = goto_points[goto_index];
+			botsMoveEntityToTarget(Entity_id_get(entity), goto_point.x, goto_point.y);
+
+			f32 dx = goto_point.x  - Vec2_x_get(position);
+			f32 dy = goto_point.y  - Vec2_y_get(position);
+			f32 dist = sqrtf(dx * dx + dy * dy);
+			if (dist < 40.0f) {
+				goto_index = (goto_index + 1) % NUM_GOTO_POINTS;
+			}
+		}
+	}
 
 	reset_buffer(&print_buffer);
-	write_string_to_buffer(&print_buffer, (const i8*) "Hello World. I am C++\n\0", 0);
-	botsLog(print_buffer.ptr, print_buffer.offset);
+	if (step_count > 0 && step_count % 100 == 0) {
+		i32 bytes_written = sprintf((char *)print_buffer.ptr, "Hello World. I am C. %d steps. %zu entities", step_count, num_entities);
+		my_assert(bytes_written >= 0);
+		print_buffer.offset += bytes_written;
+		botsLog(print_buffer.ptr, print_buffer.offset);
+	}
 
 	step_count += 1;
 }
