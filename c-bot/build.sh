d1732485d77e739274d5af84e5b2035245320fd7
streak324 <streak324@gmail.com>
2024-07-06 14:25:21 -0700

    update build script

--- c-bot/build.sh
+++ c-bot/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 #FLATCC_NO_ASSERT=1 or FLATCC_ASSERT=my_assert is required to avoid WASI imports
 emcc main.c -DFLATCC_ASSERT=my_assert -I./ -o bot.wasm \
-	-s WASM=1 -s INITIAL_HEAP=256kb -s STACK_SIZE=64kb -s ALLOW_MEMORY_GROWTH=0 \
+	-s WASM=1 -s INITIAL_HEAP=256kb -s STACK_SIZE=64kb -s ALLOW_MEMORY_GROWTH=1 -s MAXIMUM_MEMORY=0 \
 	-Wl,--stack-first -Wl,--no-demangle -Wl,--no-entry -Wl,--import-memory
 
 wasm2wat bot.wasm > bot.wat
