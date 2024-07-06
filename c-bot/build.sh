f7b5bffa3aadb8bb17a49c5140a9651f1d0b244a
streak324 <streak324@gmail.com>
2024-07-06 14:10:59 -0700

    Revert "fix to not specify max memory"
    This reverts commit db207a3a604f2fcbba502da0f2c0a8de9516fd56.

--- c-bot/build.sh
+++ c-bot/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -e
 #FLATCC_NO_ASSERT=1 or FLATCC_ASSERT=my_assert is required to avoid WASI imports
 emcc main.c -DFLATCC_ASSERT=my_assert -I./ -o bot.wasm \
-	-s WASM=1 -s INITIAL_HEAP=256kb -s STACK_SIZE=64kb \
-	-s ALLOW_MEMORY_GROWTH=1 -s MAXIMUM_MEMORY=0 \
-    -Wl,--stack-first -Wl,--no-demangle -Wl,--no-entry -Wl,--import-memory
-
+	-s WASM=1 -s INITIAL_HEAP=256kb -s STACK_SIZE=64kb -s ALLOW_MEMORY_GROWTH=0 \
+	-Wl,--stack-first -Wl,--no-demangle -Wl,--no-entry -Wl,--import-memory
 
 wasm2wat bot.wasm > bot.wat
 wasm-objdump bot.wasm -d > bot.objdump
